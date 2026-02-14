1b03100ddf6714d4f16fbf0e60388f279a416127
oguzmusk <77849428+oguzmusk@users.noreply.github.com>
2026-02-14 03:56:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Ayşe",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Sevgililer Günümüz Kutlu Olsun!! 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -22,35 +22,35 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
-            yesBtn: "Yes",                                             // Text for "Yes" button
-            noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            text: "Benden hoşlanıyor musun?",                                    // First interaction
+            yesBtn: "Evettt",                                             // Text for "Yes" button
+            noBtn: "Yoo",                                               // Text for "No" button
+            secretAnswer: "Senden hoşlanıyorum değill Seni çokk seviyorumm!! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
+            text: "Beni ne kadar çok seviyorsun?",                          // For the love meter
+            startText: "Bu kadar",                                   // Text before the percentage
+            nextBtn: "Sonraki ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            text: "Beni hep sevecek misin? 🌹", // The big question!
+            yesBtn: "Evet!!",                                             // Text for "Yes" button
+            noBtn: "Hayır"                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Beni gerçekten bu kadar çok mu seviyorsunnn?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Sonsuzaa kadarrr 🚀💝",              // Shows when they go past 1000%
+        normal: " Ve ötesinee 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Dünyadaki en şanslı insanım 🎉💝💖💝💓",
+        message: "Yan yanayken hediyeni (sarılma ve bol bol öpücükk) vereceğim.",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
